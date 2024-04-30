88af33bb51963c3a4b12f419a457423f946e6eb8
jeet4indiaa <iamjitendrameena18@gmail.com>
2024-04-30 14:54:44 +0530

    add express js

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,19 @@
+import app from "./app";
 import { Config } from "./config";
 
-// eslint-disable-next-line no-console
-console.log("PORT", Config.PORT);
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        // eslint-disable-next-line no-console
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            //logger.error(err.message);
+            // eslint-disable-next-line no-console
+            console.log(err);
+            process.exit(1);
+        }
+    }
+};
+
+startServer();
