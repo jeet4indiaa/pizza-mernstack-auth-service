7c961e37421444d4b0c66f7f37df018aeb12a222
jeet4indiaa <iamjitendrameena18@gmail.com>
2024-05-03 12:39:16 +0530

    add test for persist user

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,31 +1,25 @@
 import request from "supertest";
 import app from "../../src/app";
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../../src/config/data-source";
+import { User } from "../../src/entity/User";
+import { truncateTables } from "../utils/index";
 
 describe("POST /auth/register", () => {
-    //let connection: DataSource;
+    let connection: DataSource;
 
-    // beforeAll(async () => {
-    //     connection = await AppDataSource.initialize();
-    // });
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
 
-    // beforeEach(async () => {
-    //     // Database truncate
-    //     await connection.dropDatabase();
-    //     await connection.synchronize();
-    // });
+    beforeEach(async () => {
+        // Database truncate
+        await truncateTables(connection);
+    });
 
-    // afterAll(async () => {
-    //     try {
-    //         if (connection) {
-    //             await connection.destroy();
-    //             console.log("Connection destroyed successfully.");
-    //         } else {
-    //             console.warn("Connection is already undefined.");
-    //         }
-    //     } catch (error) {
-    //         console.error("Error occurred while destroying connection:", error);
-    //     }
-    // });
+    afterAll(async () => {
+        await connection.destroy();
+    });
 
     describe("Given all fields", () => {
         it("should return the 201 status code", async () => {
@@ -44,45 +38,45 @@ describe("POST /auth/register", () => {
             // Assert
             expect(response.statusCode).toBe(201);
         });
-    });
 
-    it("should return valid json response", async () => {
-        // Arrange
-        const userData = {
-            firstName: "Jitendra",
-            lastName: "M",
-            email: "jitendram@gmai.com",
-            password: "secret",
-        };
-        // Act
-        const response = await request(app)
-            .post("/auth/register")
-            .send(userData);
+        it("should return valid json response", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Jitendra",
+                lastName: "M",
+                email: "jitendram@gmai.com",
+                password: "secret",
+            };
+            // Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
 
-        // Assert application/json utf-8
-        expect(
-            (response.headers as Record<string, string>)["content-type"],
-        ).toEqual(expect.stringContaining("json"));
-    });
+            // Assert application/json utf-8
+            expect(
+                (response.headers as Record<string, string>)["content-type"],
+            ).toEqual(expect.stringContaining("json"));
+        });
 
-    it("should persist the user in the database", async () => {
-        // Arrange
-        const userData = {
-            firstName: "Jitendra",
-            lastName: "M",
-            email: "jitendram@gmai.com",
-            password: "secret",
-        };
-        // Act
-        await request(app).post("/auth/register").send(userData);
+        it("should persist the user in the database", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Jitendra",
+                lastName: "M",
+                email: "jitendram@gmai.com",
+                password: "secret",
+            };
+            // Act
+            await request(app).post("/auth/register").send(userData);
 
-        // Assert
-        // const userRepository = connection.getRepository(User);
-        // const users = await userRepository.find();
-        // expect(users).toHaveLength(1);
-        // expect(users[0].firstName).toBe(userData.firstName);
-        // expect(users[0].lastName).toBe(userData.lastName);
-        // expect(users[0].email).toBe(userData.email);
+            // Assert
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            expect(users).toHaveLength(1);
+            expect(users[0].firstName).toBe(userData.firstName);
+            expect(users[0].lastName).toBe(userData.lastName);
+            expect(users[0].email).toBe(userData.email);
+        });
     });
 
     describe("Fields are missing", () => {});
