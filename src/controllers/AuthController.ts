bb2babef2fa6fbe516cea7b3c38c2e7aab56fae1
jeet4indiaa <iamjitendrameena18@gmail.com>
2024-05-06 17:12:08 +0530

    refactor tokens

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -5,8 +5,6 @@ import { NextFunction } from "express-serve-static-core";
 import { Logger } from "winston";
 import { validationResult } from "express-validator";
 import { JwtPayload } from "jsonwebtoken";
-import { AppDataSource } from "../config/data-source";
-import { RefreshToken } from "../entity/RefreshToken";
 import { TokenService } from "../services/TokenService";
 
 export class AuthController {
@@ -53,13 +51,9 @@ export class AuthController {
             const accessToken = this.tokenService.generateAccessToken(payload);
 
             // Persist the refresh token
-            const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365; // 1Y
-            const refreshTokenRepository =
-                AppDataSource.getRepository(RefreshToken);
-            const newRefreshToken = await refreshTokenRepository.save({
-                user: user,
-                expiresAt: new Date(Date.now() + MS_IN_YEAR),
-            });
+
+            const newRefreshToken =
+                await this.tokenService.persistRefreshToken(user);
 
             const refreshToken = this.tokenService.generateRefreshToken({
                 ...payload,
--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -3,8 +3,12 @@ import { JwtPayload, sign } from "jsonwebtoken";
 import createHttpError from "http-errors";
 import { Config } from "../config";
 import path from "path";
+import { User } from "../entity/User";
+import { RefreshToken } from "../entity/RefreshToken";
+import { Repository } from "typeorm";
 
 export class TokenService {
+    constructor(private refreshTokenRepository: Repository<RefreshToken>) {}
     generateAccessToken(payload: JwtPayload) {
         let privateKey: Buffer;
 
@@ -39,4 +43,18 @@ export class TokenService {
 
         return refreshToken;
     }
+
+    async persistRefreshToken(user: User) {
+        const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365; // 1Y
+        const newRefreshToken = await this.refreshTokenRepository.save({
+            user: user,
+            expiresAt: new Date(Date.now() + MS_IN_YEAR),
+        });
+
+        return newRefreshToken;
+    }
+
+    // async deleteRefreshToken(tokenId: number) {
+    //     return await this.refreshTokenRepository.delete({ id: tokenId });
+    // }
 }
