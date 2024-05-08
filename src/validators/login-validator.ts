d82aeeba2754bf3357af7c2e38f0cdec1b9e19f0
jeet4indiaa <iamjitendrameena18@gmail.com>
2024-05-08 09:57:11 +0530

    add login end point

---
+++ src/validators/login-validator.ts
@@ -0,0 +1,20 @@
+import { checkSchema } from "express-validator";
+//import { body } from "express-validator";
+
+export default checkSchema({
+    email: {
+        trim: true,
+        errorMessage: "Email is required!",
+        notEmpty: true,
+        isEmail: {
+            errorMessage: "Email should be a valid email",
+        },
+    },
+    password: {
+        trim: true,
+        errorMessage: "Password is required!",
+        notEmpty: true,
+    },
+});
+
+//export default [body("email").notEmpty().withMessage("Email is required!")];
