6417bc9d1e1aa67aeedeb2ab773995eb25146dce
jeet4indiaa <iamjitendrameena18@gmail.com>
2024-05-14 18:29:19 +0530

    fix ts build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,5 +18,6 @@ module.exports = {
         "no-console": "error",
         "dot-notation": "error",
         "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/require-wait": "off",
     },
 };
